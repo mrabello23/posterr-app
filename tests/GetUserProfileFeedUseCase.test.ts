a6de3fed7e7351a0808ac39a3026db5b9d1f5501
Marcel Oliveira <m.rabello23@gmail.com>
2023-01-21 16:22:35 -0300

    created unit tests for UseCases

--- tests/GetUserProfileFeedUseCase.test.ts
+++ tests/GetUserProfileFeedUseCase.test.ts
@@ -1,11 +1,84 @@
-beforeEach(() => {
+import PostgreAdapter from "../src/infra/adapters/PostgreAdapter";
+import PostRepositoryPostgreAdapter from "../src/infra/adapters/PostRepositoryPostgreAdapter";
+import QuoteRepositoryPostgreAdapter from "../src/infra/adapters/QuoteRepositoryPostgreAdapter";
+import Post from "../src/entities/Post";
+import Quote from "../src/entities/Quote";
+import GetUserProfileFeedUseCase from "../src/usecases/GetUserProfileFeedUseCase";
+
+jest.mock("../src/infra/adapters/PostgreAdapter");
+jest.mock("../src/infra/adapters/PostRepositoryPostgreAdapter");
+jest.mock("../src/infra/adapters/QuoteRepositoryPostgreAdapter");
+
+const database = new PostgreAdapter();
+const PostRepositoryMock = new PostRepositoryPostgreAdapter(database);
+const QuoteRepositoryMock = new QuoteRepositoryPostgreAdapter(database);
+
+const dateStr = new Date().toLocaleDateString();
+
+const posts = [
+  new Post({
+    id: "post_id",
+    text: "Get UserProfile Feed UseCase",
+    user_id: "user_id",
+    repost: false,
+    created_at: dateStr,
+  }),
+];
+
+const quotes = [
+  new Quote({
+    id: "quote_id",
+    text: "Get UserProfile Feed UseCase",
+    user_id: "user_id",
+    original_post_id: "original_post_id",
+    created_at: dateStr,
+  }),
+];
+
+afterEach(() => {
   jest.clearAllMocks();
   jest.resetAllMocks();
 });
 
 describe("GetUserProfileFeedUseCase Test", () => {
   it("Should get User Profile Feed", async () => {
-    console.log("testing...");
-    expect(true).toBe(true);
+    const totalPostsByUser = 1;
+    const totalQuotesByUser = 1;
+
+    jest.spyOn(PostRepositoryMock, "getAllByUserId").mockResolvedValueOnce(posts);
+    jest.spyOn(PostRepositoryMock, "getTotalPostsByUserId").mockResolvedValueOnce(totalPostsByUser);
+
+    jest.spyOn(QuoteRepositoryMock, "getAllByUserId").mockResolvedValueOnce(quotes);
+    jest.spyOn(QuoteRepositoryMock, "getTotalByUserId").mockResolvedValueOnce(totalQuotesByUser);
+
+    const usecase = new GetUserProfileFeedUseCase(PostRepositoryMock, QuoteRepositoryMock);
+    const data = await usecase.execute({ userId: "user_id", from: dateStr, to: dateStr });
+
+    const returnData = {
+      feed: {
+        posts,
+        quotes,
+        previousPage: 1,
+        nextPage: 2,
+      },
+      userId: "user_id",
+      totalPosts: Number(totalPostsByUser) + Number(totalQuotesByUser),
+    };
+
+    expect(data).toStrictEqual(returnData);
+
+    expect(PostRepositoryMock.getAllByUserId).toBeCalledTimes(1);
+    expect(PostRepositoryMock.getTotalPostsByUserId).toBeCalledTimes(1);
+
+    expect(QuoteRepositoryMock.getAllByUserId).toBeCalledTimes(1);
+    expect(QuoteRepositoryMock.getTotalByUserId).toBeCalledTimes(1);
+  });
+
+  it("Should get error when user_id is not presented", async () => {
+    const usecase = new GetUserProfileFeedUseCase(PostRepositoryMock, QuoteRepositoryMock);
+
+    await expect(usecase.execute({ from: dateStr, to: dateStr })).rejects.toThrow(
+      "UserId is missing.",
+    );
   });
 });
--- tests/GetUserProfileUseCase.test.ts
+++ tests/GetUserProfileUseCase.test.ts
@@ -1,11 +1,42 @@
-beforeEach(() => {
+import User from "../src/entities/User";
+import PostgreAdapter from "../src/infra/adapters/PostgreAdapter";
+import UserRepositoryPostgreAdapter from "../src/infra/adapters/UserRepositoryPostgreAdapter";
+import GetUserProfileUseCase from "../src/usecases/GetUserProfileUseCase";
+
+jest.mock("../src/infra/adapters/PostgreAdapter");
+jest.mock("../src/infra/adapters/UserRepositoryPostgreAdapter");
+
+const database = new PostgreAdapter();
+const UserRepositoryMock = new UserRepositoryPostgreAdapter(database);
+
+const user = new User({
+  id: "user_id",
+  name: "name",
+  email: "email",
+  username: "username",
+  created_at: "2023-01-01",
+});
+
+afterEach(() => {
   jest.clearAllMocks();
   jest.resetAllMocks();
 });
 
 describe("GetUserProfileUseCase Test", () => {
   it("Should get User Profile", async () => {
-    console.log("testing...");
-    expect(true).toBe(true);
+    jest.spyOn(UserRepositoryMock, "getById").mockResolvedValueOnce(user);
+
+    const usecase = new GetUserProfileUseCase(UserRepositoryMock);
+    const data = await usecase.execute("user_id");
+
+    const date = new Date(user.getCreatedAt());
+    const monthString = date.toLocaleString("en-US", {
+      month: "long",
+    });
+    const createdAtFormatted = `${monthString} ${date.getDate()}, ${date.getFullYear()}`;
+    user.createdAtFormatted = createdAtFormatted;
+
+    expect(data).toStrictEqual(user);
+    expect(UserRepositoryMock.getById).toBeCalledTimes(1);
   });
 });
